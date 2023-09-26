550dec4377e7ea766abb97f54bc3f27390a12ee8
amaran-th <songsy405@naver.com>
2023-09-26 09:03:04 +0900

    feat: DI, IoC 학습 테스트

--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,18 @@
 package di.stage4.annotations;
 
+import org.reflections.Reflections;
+
+import java.util.HashSet;
 import java.util.Set;
 
 public class ClassPathScanner {
 
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        Reflections reflections = new Reflections(packageName);
+        Set<Class<?>> classes = new HashSet<>();
+        classes.addAll(reflections.getTypesAnnotatedWith(Inject.class));
+        classes.addAll(reflections.getTypesAnnotatedWith(Repository.class));
+        classes.addAll(reflections.getTypesAnnotatedWith(Service.class));
+        return classes;
     }
 }
